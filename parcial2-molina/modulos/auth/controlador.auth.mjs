1a3a56a4d7d01423deb0414e9988a91debfb2be5
JuanFeMolina <molina11@outlook.com>
2026-06-24 13:14:45 -0300

    parcial 2 molina

--- parcial2-molina/modulos/auth/controlador.auth.mjs
+++ parcial2-molina/modulos/auth/controlador.auth.mjs
@@ -1,4 +1,5 @@
 // modulos/auth/controlador.auth.mjs
+import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import authModelo from './modelo.auth.mjs'
 import env from 'dotenv'
@@ -8,30 +9,35 @@ export const login = async (req, res) => {
         const { usuario, pass } = req.body
         const usuarioEncontrado = await authModelo.buscarUsuario(usuario)
 
-        if (!usuarioEncontrado || usuarioEncontrado.password_hash !== pass) {
-            return res.status(401).json({ error: "Credenciales no válidas" })
-        }
+        // if (!usuarioEncontrado || usuarioEncontrado.password_hash !== pass) {
+        //     return res.status(401).json({ error: "Credenciales no válidas" })
+        // }
 
+        let passwordValida = false
+        if( usuarioEncontrado){
+            passwordValida = await bcrypt.compare(pass, usuarioEncontrado.password_hash)
+        }
+        
         const token = jwt.sign(
             { id: usuarioEncontrado.id, usuario: usuarioEncontrado.username },
-            process.env.FIRMA_JWT, 
+            process.env.FIRMA_JWT,
             { expiresIn: '1h' }
         )
 
         // Guardamos la sesión
-        await authModelo.guardarSesion(usuarioEncontrado.id, token)
+        // await authModelo.guardarSesion(usuarioEncontrado.id, token)
 
         // Inyectamos la cookie firmada
         res.cookie('token_parcial', token, {
-            httpOnly: true, 
-            secure: false,  
-            signed: true,   
-            maxAge: 3600000 
+            httpOnly: true,
+            secure: false,
+            signed: true,
+            maxAge: 3600000
         })
 
         // Todo OK, redirigimos
         return res.status(200).json({ mensaje: "Autenticación exitosa", redirect: "/admin" })
-         
+
     } catch (error) {
         console.error("Error en el login: ", error)
         return res.status(500).json({ error: "Error interno" })
@@ -45,7 +51,7 @@ export const salir = async (req, res) => {
             await authModelo.borrarSesion(token)
         }
         res.clearCookie('token_parcial')
-        return res.status(200).json({ redirect: "/login" }) 
+        return res.status(200).json({ redirect: "/login" })
     } catch (error) {
         return res.status(500).json({ error: "Error al cerrar sesión" })
     }
--- parcial2-molina/modulos/middlewares/authMiddleware.mjs
+++ parcial2-molina/modulos/middlewares/authMiddleware.mjs
@@ -1,34 +1,40 @@
 // modulos/middlewares/authMiddleware.mjs
 import jwt from 'jsonwebtoken'
-import AuthModel from '../auth/modelo.auth.mjs' 
+import AuthModel from '../auth/modelo.auth.mjs'
 import 'dotenv/config'
 
 export const protegerRuta = async (req, res, next) => {
-    try {
-        const token = req.signedCookies?.token_parcial
 
-        if (!token) {
-            if (req.accepts('html')) return res.redirect('/login')
-            return res.status(401).json({ error: "Acceso denegado. No se encontró un token activo." })
-        }
+    const token = req.signedCookies?.token_parcial
 
-        const usuarioVerificado = jwt.verify(token, process.env.FIRMA_JWT)
-        const sesionEnBaseDatos = await AuthModel.buscarSesionActiva(token)
-
-        if (!sesionEnBaseDatos) {
-            res.clearCookie('token_parcial')
-            if (req.accepts('html')) return res.redirect('/login')
-            return res.status(401).json({ error: "Sesión inválida o expirada en la base de datos." })
-        }
-
-        req.usuario = usuarioVerificado
-        next()
-
-    } catch (error) {
-        console.error("❌ Error de validación en el Middleware:", error.message)
-        res.clearCookie('token_parcial')
-        
+    if (!token) {
         if (req.accepts('html')) return res.redirect('/login')
-        return res.status(403).json({ error: "Token inválido, alterado o expirado." })
+        return res.status(401).json({ error: "Acceso denegado. No se encontró un token activo." })
     }
+
+    // const usuarioVerificado = jwt.verify(token, process.env.FIRMA_JWT)
+
+    jwt.verify(token, process.env.FIRMA_JWT, async (error, payload) => {
+
+        if (error) {
+            res.clearCookie('token')
+
+            // if (req.originalUrl.startsWith('/api') || req.originalUrl.startsWith('calcular-')) {
+            //     return res.status(401).json({ mensaje: 'Token invalido o expirado' })
+            // }
+
+            return res.redirect('/login')
+        }
+
+        // const sesionEnBaseDatos = await AuthModel.buscarSesionActiva(token) 
+
+        // if (!sesionEnBaseDatos) {
+        //     res.clearCookie('token_parcial')
+        //     if (req.accepts('html')) return res.redirect('/login')
+        //     return res.status(401).json({ error: "Sesión inválida o expirada en la base de datos." })
+        // }
+
+        // req.usuario = usuarioVerificado
+        next()
+    })
 }
